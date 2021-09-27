262137c822562449929aa4cc367dd17aa735e3ac
Kevin Porras <kporras07@gmail.com>
2021-09-27 16:37:57 -0600

    Add pantheon customizations.

--- web/wp-config.php
+++ web/wp-config.php
@@ -4,6 +4,11 @@
  * This file is required in the root directory so WordPress can find it.
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
+
+if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+    require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
+}
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
 require_once ABSPATH . 'wp-settings.php';
